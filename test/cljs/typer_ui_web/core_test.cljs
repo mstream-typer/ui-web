1f30a3feb7f66e08c6024666a2fab524965f76fd
Maciej Laciak <maciej.laciak@gmail.com>
2017-08-01 23:14:57 +0100

    Replaces unit tests with a spec runner.

--- test/cljs/typer_ui_web/core_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns typer-ui-web.core-test
-  (:require [clojure.test.check :as tc]
-            [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop :include-macros true]
-            [typer-ui-web.core :as core]))
-
-
--- test/cljs/typer_ui_web/runner.cljs
+++ test/cljs/typer_ui_web/runner.cljs
@@ -1,12 +1,17 @@
 (ns typer-ui-web.runner
-    (:require [doo.runner :refer-macros [doo-tests]]
-              [typer-ui-web.core-test]
-              [typer-ui-web.views-test]
-              [typer-ui-web.subs-test]
-              [typer-ui-web.events-test]))
+  (:require [doo.runner :refer-macros [doo-tests]]
+            [typer-ui-web.subs]
+            [typer-ui-web.events]
+            [typer-ui-web.views]
+            [clojure.test.check :as tc]
+            [clojure.spec.test.alpha :as stest :include-macros true]
+            [clojure.test :as test :include-macros true]))
 
 
-(doo-tests 'typer-ui-web.core-test
-           'typer-ui-web.views-test
-           'typer-ui-web.events-test
-           'typer-ui-web.subs-test)
+(test/deftest spec-tests
+  (let [result (stest/check)
+        failures (filter #(-> % ::tc/ret :result not) result)]
+    (test/is (empty? failures))))
+
+
+(doo-tests)
